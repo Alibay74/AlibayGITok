9e9653e6935ce8400c93a1496faf7f8b85db9256
Alibay74 <aitov.74@list.ru>
2021-01-13 20:11:16 -0500

    gggg

---
+++ agitas.js
@@ -0,0 +1,19 @@
+
+public static void main(String[]args){
+
+    String name = "Alibay";
+    String lastname = "Aitov";
+    int age = 46;
+    String Card # = "1111-2222-3333-4444";
+    String Issue = "11/22";
+    String cvc = "123";
+}
+    System.out.print("My name " +name);
+    System.out.print("My lasname " +lastname);
+    System.out.print("My age " +age);
+    System.out.print("Debit card " +Card);
+    System.out.print("Date issue " +Issue);
+    System.out.print("Bankcvc " +cvc);
+    //System.out.print("cvc " +cvc);
+
+    
